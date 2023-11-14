47c023bd4e6397937b9e8fd1f34a7314b2d1d85f
naveen sanjula <naveensanjula975@gmail.com>
2023-11-14 18:01:43 +0530

    Updates

--- src/helpers/useChangeDocumentTitle.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-
-export const useChangeDocumentTitle = (pageTitle) => {
-	useEffect(() => {
-		document.title = `Eduard-Constantin Ibinceanu | Personal Portfolio - ${pageTitle}`;
-	}, [pageTitle]);
-};
