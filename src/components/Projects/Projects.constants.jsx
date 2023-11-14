e02d932b6fc0ec4f4cfc8c3895b3192a91fede28
naveen sanjula <naveensanjula975@gmail.com>
2023-11-14 18:34:49 +0530

    update

---
+++ src/components/Projects/Projects.constants.jsx
@@ -0,0 +1,194 @@
+import webLink from '../../assets/images/links/globe2.svg';
+import dribbbleLink from '../../assets/images/links/dribbble.svg';
+import gitLink from '../../assets/images/links/github.svg';
+import ytLink from '../../assets/images/links/youtube.svg';
+
+import jdsoftware from '../../assets/images/projects/jdsoftware.png';
+import portfolio from '../../assets/images/projects/portfolio.png';
+import livrator from '../../assets/images/projects/livrator.png';
+import ghicicuvantul from '../../assets/images/projects/gc.png';
+import cryptocurrency from '../../assets/images/projects/cc.png';
+import traveldeck from '../../assets/images/projects/td.png';
+import xboxdash from '../../assets/images/projects/xd.png';
+
+import geex from '../../assets/images/projects/geex.png';
+import tla from '../../assets/images/projects/tla.png';
+import arbachelor from '../../assets/images/projects/bachelor.png';
+import arlgtv from '../../assets/images/projects/arlg.png';
+import ardissertation from '../../assets/images/projects/dissertation.png';
+import arop7 from '../../assets/images/projects/arop7.png';
+import castleescape from '../../assets/images/projects/castleescape.png';
+
+import smarthome from '../../assets/images/projects/smarthome.png';
+import godofwar from '../../assets/images/projects/godofwar.png';
+import itlogo from '../../assets/images/projects/intimelogo.png';
+import nyoologo from '../../assets/images/projects/ntoologo.png';
+import btnanim from '../../assets/images/projects/btnAnim.png';
+import navui from '../../assets/images/projects/navui.png';
+import mplogo from '../../assets/images/projects/mplogo.png';
+import discoverro from '../../assets/images/projects/discover-ro.png';
+import vclp from '../../assets/images/projects/vclp.png';
+import vibenow from '../../assets/images/projects/vibenow.png';
+import dynamicmenu from '../../assets/images/projects/dynamicmenu.png';
+
+export const web = [
+	{
+		name: 'Portfolio',
+		date: '2022-05-08',
+		cover: portfolio,
+		links: [{ icon: gitLink, link: '' }],
+	},
+	{
+		name: '',
+		date: '2022-04-30',
+		// cover: livrator,
+		links: [{ icon: webLink, link: 'https://www.' }],
+	},
+	{ name: 'Ghiceste cuvantul',
+		date: '2022-03-20',
+		// cover: ghicicuvantul,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/' }] },
+	{
+		name: 'Xbox Dashboard',
+		date: '2022-03-06',
+		// cover: xboxdash,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/' }],
+	},
+	{
+		name: 'Crypto Currencies',
+		date: '2021-10-21',
+		// cover: cryptocurrency,
+		links: [
+			{ icon: gitLink, link: 'https://github.com/' },
+			{ icon: dribbbleLink, link: 'https://dribbble.com/' },
+		],
+	},
+	{
+		name: 'Travel Deck',
+		date: '2021-11-16',
+		// cover: traveldeck,
+		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/' }],
+	},
+	{
+		name: 'Navigation Menu Animation',
+		date: '2022-03-20',
+		cover: navui,
+		links: [
+			{ icon: gitLink, link: 'https://github.com/' },
+			{ icon: dribbbleLink, link: 'https://dribbble.com/s' },
+		],
+	},
+	{
+		name: 'Button Animation',
+		date: '2022-05-01',
+		cover: btnanim,
+		links: [
+			{ icon: gitLink, link: 'https://github.com/' },
+			{ icon: dribbbleLink, link: 'https://dribbble.com/' },
+		],
+	},
+	{
+		name: 'JD Software',
+		date: '2022-08-14',
+		cover: jdsoftware,
+		links: [{ icon: webLink, link: '#' }],
+	},
+	{
+		name: 'Dynamic Menu',
+		date: '2022-09-18',
+		cover: dynamicmenu,
+		links: [
+			{ icon: gitLink, link: '#' },
+			{ icon: dribbbleLink, link: '#' }
+		],
+	},
+];
+
+export const design = [
+	{
+		name: 'Smart Home Dashboard UI',
+		date: '2022-04-03',
+		cover: smarthome,
+		links: [{ icon: dribbbleLink, link: '#' }],
+	},
+	{
+		name: 'God of War Concept UI',
+		date: '2022-03-13',
+		cover: godofwar,
+		links: [{ icon: dribbbleLink, link: '#' }],
+	},
+	{
+		name: 'In time Logo',
+		date: '2020-04-20',
+		cover: itlogo,
+		links: [{ icon: dribbbleLink, link: '#' }],
+	},
+	{
+		name: 'NY Open Offices Logo',
+		date: '2018-05-12',
+		cover: nyoologo,
+		links: [{ icon: dribbbleLink, link: '#' }],
+	},
+	{
+		name: 'Midnight Pulse Logo',
+		date: '2018-07-17',
+		cover: mplogo,
+		links: [{ icon: dribbbleLink, link: '#' }],
+	},
+	{
+		name: 'Discover Romania Concept UI',
+		date: '2022-06-05',
+		cover: discoverro,
+		links: [
+			{ icon: dribbbleLink, link: '#' },
+			{ icon: ytLink, link: '#' },
+		],
+	},
+	{
+		name: 'Vice City Conceput UI',
+		date: '2022-08-14',
+		cover: vclp,
+		links: [
+			{ icon: dribbbleLink, link: '#' },
+			{ icon: ytLink, link: '#' },
+		],
+	},
+	{
+		name: 'Vibe Now UI',
+		date: '2022-10-28',
+		cover: vibenow,
+		links: [
+			{ icon: dribbbleLink, link: '#' },
+		],
+	},
+];
+
+export const game = [
+	{ name: 'Ge Ex', date: '2014-09-01', cover: geex, links: [] },
+	{
+		name: 'The Last Ancient',
+		date: '2017-05-01',
+		cover: tla,
+		links: [{ icon: webLink, link: '#' }],
+	},
+	{ name: "Bachelor's degree AR project", date: '2018-07-20', cover: arbachelor, links: [] },
+	{ name: "Master's degree AR project", date: '2020-07-20', cover: ardissertation, links: [] },
+	{
+		name: 'LG Rollable TV AR project',
+		date: '2019-01-23',
+		cover: arlgtv,
+		links: [{ icon: ytLink, link: '#' }],
+	},
+	{
+		name: 'Oneplus 7 AR project',
+		date: '2019-05-21',
+		cover: arop7,
+		links: [{ icon: ytLink, link: '#' }],
+	},
+	{
+		name: 'Castle Escape',
+		date: '2018-12-20',
+		cover: castleescape,
+		links: [{ icon: ytLink, link: '#' }],
+	},
+];
