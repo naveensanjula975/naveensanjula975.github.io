b8bafbd6a1adb9dc6ecb66afb817e4cdffd815db
naveen sanjula <naveensanjula975@gmail.com>
2023-11-27 15:53:53 +0530

    full upgrade

--- src/components/Menu/SocialIcons/SocialIcons.constants.jsx
+++
@@ -1,9 +0,0 @@
-// import cv from "./CV.pdf";
-
-export const socialLinks = [
-	{ name: 'Twitter', link: 'https://twitter.com/naveensanjula20' },
-	{ name: 'Dribbble', link: 'https://dribbble.com/naveensanjula' },
-	{ name: 'Github', link: 'https://github.com/naveensanjula975' },
-	// {name: "Resume", link: cv},
-	{ name: 'Mail', link: 'mailto: naveensanjula15@gmail.com' },
-];
